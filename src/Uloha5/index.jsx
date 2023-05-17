141e9e704e48d7df36ce26407c11519133f77a39
Kamila Semotánová <kamila@Kamilas-MacBook-Air.local>
2023-05-17 18:53:06 +0200

    solution to task 5

--- src/Uloha5/index.jsx
+++ src/Uloha5/index.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 // Zadání 1: Dopiš komponentu `NakupniPolozka`, aby v divu byl vypsaný název položky a tlačítko s textem „koupit“.
 // Zadání 2: Po kliknutí na tlačítko vypiš do konzole „Uživatel chce koupit DOPLŇ_NÁZEV_POLOŽKY“.
 
 export const NakupniPolozka = ({ nazev }) => {
-	return <div>@TODO</div>
-}
+  return (
+    <div>
+      <h5>{nazev}</h5>
+      <button
+        onClick={() => {
+          console.log(`Uživatel chce koupit ${nazev}`);
+        }}
+      >
+        Koupit
+      </button>
+    </div>
+  );
+};
 
 // Zadání 3: Pomocí `polozky.map` vypiš všechny názvy jako nákupní položky.
 
 // Bonus: Nastav jednotlivým položkám ve výpisu `key`, aby vývojářské nástroje v prohlížeči nezobrazovaly varování. Nápověda: Key musí být v seznamu unikátní. Použij název položky.
 
 export const Uloha5 = () => {
-	const polozky = ['hrušky', 'jablka', 'třešně', 'jahody']
+  const polozky = ['hrušky', 'jablka', 'třešně', 'jahody'];
 
-	return (
-		<>
-			<NakupniPolozka nazev="hrušky" />
-			<NakupniPolozka nazev="jablka" />
-		</>
-	)
-}
+  const list = polozky.map((ovoce) => {
+    return <NakupniPolozka nazev={ovoce} key={ovoce} />;
+  });
+  return list;
+
+  // <>
+  //   <NakupniPolozka nazev="hrušky" />
+  //   <NakupniPolozka nazev="jablka" />
+  // </>
+};
